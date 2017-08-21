85d75c9ee8568025ae1e39e55a9475344db611bb
Jamie Strandboge <jamie@ubuntu.com>
2017-08-21 16:41:59 -0500

    clickreviews/sr_functional.py: fix typo in join

--- clickreviews/sr_functional.py
+++ clickreviews/sr_functional.py
@@ -61,7 +61,7 @@ class SnapReviewFunctional(SnapReview):
 
         if len(bins) > 0:
             t = 'error'
-            s = "Found files with executable stack. This adds PROT_EXEC to mmap(2) during mediation which may cause security denials. Either adjust your program to not require an executable stack or strip it with 'execstack --clear-execstack ...'. Affected files: %s" % " ,".join(bins)
+            s = "Found files with executable stack. This adds PROT_EXEC to mmap(2) during mediation which may cause security denials. Either adjust your program to not require an executable stack or strip it with 'execstack --clear-execstack ...'. Affected files: %s" % ", ".join(bins)
             link = 'https://forum.snapcraft.io/t/file-mmap-of-dev-zero-is-currently-blocked/1426/21'
 
         self._add_result(t, n, s, link=link)
