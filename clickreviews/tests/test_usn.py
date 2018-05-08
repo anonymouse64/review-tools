8065dc4e9c4f0b693562d5bf0b73816c45a7b4c6
Jamie Strandboge <jamie@ubuntu.com>
2018-05-08 10:27:43 -0500

    clickreviews/tests/test_usn.py: bring tests to 100% coverage

--- clickreviews/tests/test_usn.py
+++ clickreviews/tests/test_usn.py
@@ -15,14 +15,22 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from unittest import TestCase
+from unittest.mock import patch
 
 import clickreviews.usn as usn
+import clickreviews.common as common
 
 
 class TestUSN(TestCase):
     """Tests for the USN functions."""
 
-    def test_check_read_usn_dn(self):
+    def patch_json_load(self, d):
+        p = patch('clickreviews.common.json.load')
+        self.mock_load = p.start()
+        self.mock_load.return_value = d
+        self.addCleanup(p.stop)
+
+    def test_check_read_usn_db(self):
         '''Test read_usn_db()'''
         res = usn.read_usn_db("./tests/test-usn-unittest-1.db")
 
@@ -61,3 +69,157 @@ class TestUSN(TestCase):
                     self.assertTrue(sn in res[rel][pkg])
                     self.assertEquals(expected_db[rel][pkg][sn],
                                       str(res[rel][pkg][sn]))
+
+    def test_check_read_usn_db_no_release(self):
+        '''Test read_usn_db() - no releases'''
+        # mock up the usn db (for simplicity, we read an existing one then
+        # modify it)
+        raw = common.read_file_as_json_dict("./tests/test-usn-unittest-1.db")
+
+        # delete USNs we don't care about
+        usns = list(raw.keys())
+        for k in usns:
+            if k != '3606-1':
+                del raw[k]
+
+        # modify the USN
+        del raw['3606-1']['releases']
+
+        # mock up returning raw when reading ./tests/test-usn-unittest-1.db
+        # with json load
+        self.patch_json_load(raw)
+        res = usn.read_usn_db("./tests/test-usn-unittest-1.db")
+        self.assertEquals(len(res), 0)
+
+    def test_check_read_usn_db_no_xenial(self):
+        '''Test read_usn_db() - no xenial'''
+        # mock up the usn db (for simplicity, we read an existing one then
+        # modify it)
+        raw = common.read_file_as_json_dict("./tests/test-usn-unittest-1.db")
+
+        # delete USNs we don't care about
+        usns = list(raw.keys())
+        for k in usns:
+            if k != '3606-1':
+                del raw[k]
+
+        # modify the USN
+        del raw['3606-1']['releases']['xenial']
+
+        # mock up returning raw when reading ./tests/test-usn-unittest-1.db
+        # with json load
+        self.patch_json_load(raw)
+        res = usn.read_usn_db("./tests/test-usn-unittest-1.db")
+        self.assertEquals(len(res), 0)
+
+    def test_check_read_usn_db_no_sources(self):
+        '''Test read_usn_db() - no sources'''
+        # mock up the usn db (for simplicity, we read an existing one then
+        # modify it)
+        raw = common.read_file_as_json_dict("./tests/test-usn-unittest-1.db")
+
+        # delete USNs we don't care about
+        usns = list(raw.keys())
+        for k in usns:
+            if k != '3606-1':
+                del raw[k]
+
+        # modify the USN
+        del raw['3606-1']['releases']['xenial']['sources']
+
+        # mock up returning raw when reading ./tests/test-usn-unittest-1.db
+        # with json load
+        self.patch_json_load(raw)
+        res = usn.read_usn_db("./tests/test-usn-unittest-1.db")
+        self.assertEquals(len(res), 1)
+        self.assertEquals(len(res['xenial']), 0)
+
+    def test_check_read_usn_db_no_versions(self):
+        '''Test read_usn_db() - no versions'''
+        # mock up the usn db (for simplicity, we read an existing one then
+        # modify it)
+        raw = common.read_file_as_json_dict("./tests/test-usn-unittest-1.db")
+
+        # delete USNs we don't care about
+        usns = list(raw.keys())
+        for k in usns:
+            if k != '3606-1':
+                del raw[k]
+
+        # modify the USN
+        for src in raw['3606-1']['releases']['xenial']['sources']:
+            del raw['3606-1']['releases']['xenial']['sources'][src]['version']
+
+        # mock up returning raw when reading ./tests/test-usn-unittest-1.db
+        # with json load
+        self.patch_json_load(raw)
+        res = usn.read_usn_db("./tests/test-usn-unittest-1.db")
+        self.assertEquals(len(res), 1)
+        self.assertEquals(len(res['xenial']), 6)
+
+    def test_check_read_usn_db_no_binaries(self):
+        '''Test read_usn_db() - no binaries'''
+        # mock up the usn db (for simplicity, we read an existing one then
+        # modify it)
+        raw = common.read_file_as_json_dict("./tests/test-usn-unittest-1.db")
+
+        # delete USNs we don't care about
+        usns = list(raw.keys())
+        for k in usns:
+            if k != '3606-1':
+                del raw[k]
+
+        # modify the USN
+        del raw['3606-1']['releases']['xenial']['binaries']
+
+        # mock up returning raw when reading ./tests/test-usn-unittest-1.db
+        # with json load
+        self.patch_json_load(raw)
+        res = usn.read_usn_db("./tests/test-usn-unittest-1.db")
+        self.assertEquals(len(res), 1)
+        self.assertEquals(len(res['xenial']), 0)
+
+    def test_check_read_usn_db_no_archs(self):
+        '''Test read_usn_db() - no archs'''
+        # mock up the usn db (for simplicity, we read an existing one then
+        # modify it)
+        raw = common.read_file_as_json_dict("./tests/test-usn-unittest-1.db")
+
+        # delete USNs we don't care about
+        usns = list(raw.keys())
+        for k in usns:
+            if k != '3606-1':
+                del raw[k]
+
+        # modify the USN
+        del raw['3606-1']['releases']['xenial']['archs']
+
+        # mock up returning raw when reading ./tests/test-usn-unittest-1.db
+        # with json load
+        self.patch_json_load(raw)
+        res = usn.read_usn_db("./tests/test-usn-unittest-1.db")
+        self.assertEquals(len(res), 1)
+        self.assertEquals(len(res['xenial']), 0)
+
+    def test_check_read_usn_db_no_urls(self):
+        '''Test read_usn_db() - no urls'''
+        # mock up the usn db (for simplicity, we read an existing one then
+        # modify it)
+        raw = common.read_file_as_json_dict("./tests/test-usn-unittest-1.db")
+
+        # delete USNs we don't care about
+        usns = list(raw.keys())
+        for k in usns:
+            if k != '3606-1':
+                del raw[k]
+
+        # modify the USN
+        for arch in raw['3606-1']['releases']['xenial']['archs']:
+            del raw['3606-1']['releases']['xenial']['archs'][arch]['urls']
+
+        # mock up returning raw when reading ./tests/test-usn-unittest-1.db
+        # with json load
+        self.patch_json_load(raw)
+        res = usn.read_usn_db("./tests/test-usn-unittest-1.db")
+        self.assertEquals(len(res), 1)
+        self.assertEquals(len(res['xenial']), 0)
