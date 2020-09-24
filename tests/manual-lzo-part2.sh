215c75abc8ac738e2a94ba5d7084417b5f11d973
Ian Johnson <ian.johnson@canonical.com>
2020-09-24 14:08:47 -0500

    tests/manual-lzo-part2.sh: retry apt ops if they fail
    We keep seeing both in this script and in snapd spread tests bugs where apt
    calls made sufficiently fast enough inside a focal lxd container fail with
    something like:
    
    Type 'bionic-security' is not known on line 50 in source list /etc/apt/sources.list
    
    which is somehow resolved by just running the same command again. My best bet is
    that this is a race condition in the cloud-init stuff that is running when the
    container starts, so an alternative solution could be to wait for cloud-init
    inside the container to finish, and then run these commands, but this was not
    thoroughly debugged.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- tests/manual-lzo-part2.sh
+++ tests/manual-lzo-part2.sh
@@ -118,8 +118,10 @@ for series in 16 18 20; do
 
     sudo "$LXC" file push review-tools.tgz "${cont_name}/root/review-tools.tgz"
 
-    sudo "$LXC" exec "${cont_name}" -- apt update
-    sudo "$LXC" exec "${cont_name}" -- apt upgrade -y
+    # sometimes apt operations fail due to apt parsing, probably a race 
+    # condition, but keep trying
+    sudo "$LXC" exec "${cont_name}" -- /bin/bash -c 'until apt update; do sleep 0.1; done'
+    sudo "$LXC" exec "${cont_name}" -- /bin/bash -c 'until apt upgrade -y; do sleep 0.1; done'
 
     # dependencies for review-tools as per the snapcraft.yaml
     sudo "$LXC" exec "${cont_name}" -- apt install -y binutils fakeroot file libdb5.3 libmagic1 python3-magic python3-requests python3-simplejson python3-yaml squashfs-tools
